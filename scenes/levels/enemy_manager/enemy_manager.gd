2457b6cbec73782afbc41c72a51c7ee187026d55
lextum <Lextum@users.noreply.github.com>
2026-01-31 23:22:53 +0100

    Add boss

--- scenes/levels/enemy_manager/enemy_manager.gd
+++ scenes/levels/enemy_manager/enemy_manager.gd
@@ -4,15 +4,29 @@ extends Node
 
 @export var mask: Node3D
 @export var enemy_container: Node
+@export var boss_enemy_scene: PackedScene
+
+var _boss_spawned = false
 
 
 func _ready() -> void:
 	assert(mask != null, "mask not onfigured!")
 	assert(enemy_container != null, "enemy_container not onfigured!")
+	assert(boss_enemy_scene != null, "boss enemy scene is empty stupid")
 	mask.hide()
 	enemy_container.child_order_changed.connect(_on_enemy_died)
 
 
 func _on_enemy_died() -> void:
 	if enemy_container.get_child_count() == 0:
-		mask.show()
+		if not _boss_spawned:
+			_spawn_boss()
+		else:
+			mask.show()
+
+
+func _spawn_boss() -> void:
+	var boss_enemy: CharacterBody3D = boss_enemy_scene.instantiate()
+	boss_enemy.position = mask.position
+	enemy_container.add_child(boss_enemy)
+	_boss_spawned = true
