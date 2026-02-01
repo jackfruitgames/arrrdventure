c388de44554737e173049a538e7695476e52bb59
Jan Bucher <jan@bucher.cloud>
2026-02-01 11:43:13 +0100

    Update firemask and some other stuff

--- scenes/map/island.gd
+++ scenes/map/island.gd
@@ -16,7 +16,6 @@ func _ready() -> void:
 
 
 func _on_body_entered(body: Node3D) -> void:
-	print("Collide")
 	print("Unlocked_level: " + str(GlobalState.unlocked_level) + ", this island has " + str(islandLevel))
 	if body.is_in_group("player") and islandLevel == GlobalState.unlocked_level:
 		print("Start next level!")
