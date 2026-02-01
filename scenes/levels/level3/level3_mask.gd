2a410052ca066ab6b2539ab5a0315cfde3806184
Jan Bucher <jan@bucher.cloud>
2026-02-01 10:34:04 +0100

    mask collider is only active if it is visible

--- scenes/levels/level3/level3_mask.gd
+++ scenes/levels/level3/level3_mask.gd
@@ -1,5 +1,5 @@
 extends Area3D
 
 func _on_player_entered(body: Node3D) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and get_parent_node_3d().is_visible_in_tree():
 		GlobalSignals.finish_level.emit(E.Level.Level3)
